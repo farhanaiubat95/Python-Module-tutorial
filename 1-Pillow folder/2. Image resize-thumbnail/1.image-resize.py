8b312799850411b3cbe20b67b70196c165b55639
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2024-09-07 23:58:50 +0600

    change name folder 2

--- 1-Pillow folder/2. Image resize-thumbnail/1.image-resize.py
+++ 1-Pillow folder/2. Image resize-thumbnail/1.image-resize.py
@@ -1,10 +1,10 @@
 from PIL import Image
 
-img = Image.open("1-Pillow folder/2. Image resize/Ayman.jpg")
+img = Image.open("1-Pillow folder/2. Image resize-thumbnail/Ayman.jpg")
 img.show()
 print("First image size : ", img.size)
 
 new_img_size = (400, 200)
 img.thumbnail(new_img_size)
-img.save('1-Pillow folder/2. Image resize/new_ayman.jpg')
+img.save('1-Pillow folder/2. Image resize-thumbnail/new_ayman.jpg')
 print("New image size : ", img.size)
