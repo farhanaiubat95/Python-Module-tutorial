ded3ea9c6d72a60d5165d46ea042472514edc33a
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2024-09-08 20:59:32 +0600

    create 1.image filter file

---
+++ 1-Pillow folder/6.Image Filter/1.image-filter.py
@@ -0,0 +1,16 @@
+from PIL import Image, ImageFilter
+
+img = Image.open("1-Pillow folder/6.Image Filter/flower.jpg")
+img.show()
+
+# img.filter(ImageFilter.BLUR).show()
+# img.filter(ImageFilter.MinFilter).show()  # same as MinFilter(3)
+# img.filter(ImageFilter.CONTOUR).show()
+# img.filter(ImageFilter.DETAIL).show()
+# img.filter(ImageFilter.EDGE_ENHANCE).show()
+# img.filter(ImageFilter.EDGE_ENHANCE_MORE).show()
+# img.filter(ImageFilter.EMBOSS).show()
+# img.filter(ImageFilter.FIND_EDGES).show()
+# img.filter(ImageFilter.SHARPEN).show()
+# img.filter(ImageFilter.SMOOTH).show()
+img.filter(ImageFilter.SMOOTH_MORE).show()
