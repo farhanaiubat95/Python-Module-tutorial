729eae7c489b1224b49309538d5f74b0634938f7
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2024-09-08 20:58:36 +0600

    added croped image

--- 1-Pillow folder/3.image crop/1.image_crop.py
+++ 1-Pillow folder/3.image crop/1.image_crop.py
@@ -6,6 +6,6 @@ print("First image size : ", img.size)
 
 area = (200, 60, 650, 700)  # (x1,y1,x2,y2)
 image_crop = img.crop(area)
-image_crop.save('Croped image.jpg')
+image_crop.save('1-Pillow folder/3.image crop/Croped image.jpg')
 print("After cropping : ", image_crop.size)
 image_crop.show()
