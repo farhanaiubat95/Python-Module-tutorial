d30cbbaec011ad42f11fdbbf30f15f3df202eac2
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2024-09-09 00:17:34 +0600

    created read-csv file

---
+++ 2-Pandas library for Dataframe/3.Pandas Read CSV/1.read-csv.py
@@ -0,0 +1,19 @@
+# A simple way to store big data sets is to use CSV files (comma separated files).
+# CSV files contains plain text and is a well know format that can be read by everyone including Pandas.
+import pandas as pd
+
+dataframe = pd.read_csv(
+    r'G:\Python-Module-tutorial\2-Pandas library for Dataframe\3.Pandas Read CSV\data.csv')
+print(dataframe)
+print()
+print(dataframe.to_string())  # use to_string() to print the entire DataFrame.
+
+
+# another way - if you don't want to use r(read)
+# dataframe = pd.read_csv(
+#    'G:\\Python-Module-tutorial\\2-Pandas library for Dataframe\\3.Pandas Read CSV\\data.csv')
+
+
+# Maximum rows- can be changed
+pd.options.display.max_rows = 10000
+print("\n** Maximum rows of this data table is : ", pd.options.display.max_rows)
---
+++ 2-Pandas library for Dataframe/3.Pandas Read CSV/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+# dataframe = pd.read_csv(
+#     'G:\\Python-Module-tutorial\\2-Pandas library for Dataframe\\3.Pandas Read CSV\\data.csv')
