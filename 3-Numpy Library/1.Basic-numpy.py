3db076a067c58d5220b1447441a9b1cf7918185b
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2024-09-14 15:20:38 +0600

    created basic-numpy file

---
+++ 3-Numpy Library/1.Basic-numpy.py
@@ -0,0 +1,11 @@
+# NumPy is a Python library used for working with arrays.
+# functions for working in domain of linear algebra, fourier transform, and matrices.
+# The array object in NumPy is called ndarray
+# NumPy arrays are stored at one continuous place in memory unlike lists, so processes can access and manipulate them very efficiently.
+# This is the main reason why NumPy is faster than lists.
+
+import numpy as np
+
+array = np.array([1, 2, 3, 4])
+print(array)
+print("Version name of numpy : ", np.__version__)
