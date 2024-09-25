8f4b6e13e40bdd50a3533c0cac7974e84638a58a
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2024-09-25 23:59:46 +0600

    create 4-np-normal-distribution.py

---
+++ 6-NumPy Random/4-np-normal-distribution.py
@@ -0,0 +1,20 @@
+# random.normal() method to get a Normal Data Distribution.
+# It has three parameters:
+# loc   - (Mean) where the peak of the bell exists.
+# scale - (Standard Deviation) how flat the graph distribution should be.
+# size  - The shape of the returned array.
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+array = random.normal(loc=1, scale=2, size=(3, 4))
+print('1 - a random normal distribution of size 3x4 :\n', array)
+
+
+# showing seaborn plot
+sns.distplot(array, hist=False)
+plt.show()
+
+sns.distplot(random.normal(size=500), hist=False)
+plt.show()
+# The curve of a Normal Distribution is also known as the Bell Curve because of the bell-shaped curve.
