186ff73c86e7cf0d4250ae26d67a1c23689bf39f
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2024-09-27 21:16:23 +0600

    update poisson-distribution.py

--- 6-NumPy Random/6-Poisson-Distribution.py
+++ 6-NumPy Random/6-Poisson-Distribution.py
@@ -5,7 +5,30 @@
 # size - The shape of the returned array.
 
 from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-array = random.poisson(lam=2, size=10)
+array = random.poisson(lam=2, size=1000)
+print('1 - The poisson array where lam = 2, size = 1000 : ', array)
 
-print('1 - The poisson array where lam = 2, size = 10 : ', array)
+
+sns.distplot(array, kde=False)
+plt.show()
+
+# Difference Between Normal and Poisson Distribution
+# Normal distribution is continuous whereas poisson is discrete.
+sns.distplot(random.normal(loc=50, scale=7, size=1000),
+             hist=False, label='normal')
+array = random.poisson(lam=50, size=1000)
+sns.distplot(array, hist=False, label='poisson')
+plt.show()
+
+
+# Difference Between Binomial and Poisson Distribution
+# Binomial distribution only has two possible outcomes, whereas poisson distribution can have unlimited possible outcomes.
+# But for very large n and near-zero p binomial distribution is near identical to poisson distribution such that n * p is nearly equal to lam.
+sns.distplot(random.binomial(n=1000, p=0.01, size=1000),
+             hist=False, label='binomial')
+array = random.poisson(lam=10, size=1000)
+sns.distplot(array, hist=False, label='poisson')
+plt.show()
