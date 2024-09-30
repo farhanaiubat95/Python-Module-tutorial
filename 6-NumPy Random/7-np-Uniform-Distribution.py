719e5047fafd1f58c7e4aefdc499e5df67121d6a
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2024-09-30 22:09:51 +0600

    Update 7-np-Uniform-distribution.py

--- 6-NumPy Random/7-np-Uniform-Distribution.py
+++ 6-NumPy Random/7-np-Uniform-Distribution.py
@@ -4,3 +4,9 @@
 # a - lower bound - default 0 .0.
 # b - upper bound - default 1.0.
 # size - The shape of the returned array.
+
+from numpy import random
+
+array = random.uniform(size=(2, 3))
+
+print('1 - Original array', array)
